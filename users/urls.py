7e5696219123520f9f16fcd2d0b62de458eb0e0f
Kai Siang <kslui99@gmail.com>
2022-10-05 18:20:40 +0800

    feat: implement logout

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     re_path('register/?$', views.RegisterView.as_view(), name='register'),
     re_path('login/?$', views.LoginView.as_view(), name='login'),
-    # re_path('logout/?$', jwt_views.TokenBlacklistView.as_view(), name='logout'),
+    re_path('logout/?$', jwt_views.TokenBlacklistView.as_view(), name='logout'),
     # re_path('token/refresh/?$', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     # re_path('token/verify/?$', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
 ]
