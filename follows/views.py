e73da54dc00a0a486d03e9035ee34c9ee265d8f2
Bernard Wan <bernardwan99@gmail.com>
2022-10-22 22:46:12 +0800

    Add GET follows

--- follows/views.py
+++ follows/views.py
@@ -2,12 +2,41 @@ from django.shortcuts import render
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 
 from users.models import User
+from users.serializers import UserSerializer
 from .models import Follow
 
 class FollowActionsView(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            response = Response("There is no user with the given username.", status=status.HTTP_400_BAD_REQUEST)
+            return response
+        
+        followers = request.query_params.get('followers')
+
+
+
+        if followers:
+            users = Follow.objects.filter(followee=request.user).values_list('follower', flat=True)
+        else:
+            users = Follow.objects.filter(follower=request.user).values_list('followee', flat=True)
+
+        users = User.objects.filter(id__in=users)
+        paginator = PageNumberPagination()
+
+        queryset = paginator.paginate_queryset(users, request)
+        serializer = UserSerializer(queryset, many=True, context={'request_user': request.user})
+        response = Response(serializer.data)
+        response.headers = {
+            'Pages': str(paginator.page.paginator.num_pages),
+            'Access-Control-Expose-Headers': '*'}
+        return Response(serializer.data)
 
     def post(self, request, username):
         try:
@@ -17,6 +46,9 @@ class FollowActionsView(APIView):
             return response
         
         follower = request.user
+
+        if follower == followee:
+            return Response("Cannot follow yourself", status=status.HTTP_401_UNAUTHORIZED)
 
         follow = Follow.objects.get_or_create(followee=followee, follower=follower)
 
